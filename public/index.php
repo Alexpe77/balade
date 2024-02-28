1e5cba256d70b48d62378db71d8c98e377fad85c
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2024-02-28 15:24:23 +0100

    Modify welcome image and font

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-// $url = isset($_GET['url']) ? $_GET['url'] : '';
-
 $request_uri = $_SERVER['REQUEST_URI'];
 $base_url = str_replace("/index.php", "", $_SERVER['PHP_SELF']);
 $route = str_replace($base_url, "", $request_uri);
